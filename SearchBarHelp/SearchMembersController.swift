4247fea463f91f642ba2497aec27987d799c69e1
David Verbenyi <dverbenyi@gmail.com>
2018-06-28 13:32:52 +0100

    sample members in second view controller

--- SearchBarHelp/SearchMembersController.swift
+++ SearchBarHelp/SearchMembersController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SearchMembersController: UITableViewController, UISearchControllerDelegate {
 
+    var members = ["First", "Second", "Third", "Fourth"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,17 +28,14 @@ class SearchMembersController: UITableViewController, UISearchControllerDelegate
 
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return members.count
     }
-
-
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = members[indexPath.row]
+        
+        return cell
+    }
 }
